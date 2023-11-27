ae5edaba383f66906b3688e4b825942e3e9cd68f
jogang <jogang0304@gmail.com>
2023-11-27 14:29:21 +0300

    changed start

--- start.py
+++ start.py
@@ -1,9 +1,8 @@
-import uvicorn
 import dotenv
+import uvicorn
 
 dotenv.load_dotenv()
 from backend import app
-
 
 if __name__ == "__main__":
     config = uvicorn.Config(app, port=8000, log_level="info")
